4f989035d0278868759c532539b3ac9b916d2e54
t0m <bobtfish@bobtfish.net>
2009-07-05 23:36:11 +0100

    Add Authentication and RenderView to TestApp

--- t/lib/TestApp.pm
+++ t/lib/TestApp.pm
@@ -1,9 +1,36 @@
 package TestApp;
-use strict;
-use warnings;
+use Moose;
+use namespace::autoclean;
 
-use Catalyst;
+use Catalyst qw/
+    Authentication
+/;
+extends 'Catalyst';
+
+__PACKAGE__->config(
+    'Plugin::Authentication' => {
+        default => {
+            credential => {
+                class => 'Password',
+                password_field => 'password',
+                password_type => 'clear'
+            },
+            store => {
+                class => 'Minimal',
+                users => {
+                    bob => {
+                        password => "s00p3r",
+                    },
+                    william => {
+                        password => "s3cr3t",
+                    },
+                },
+            },
+        },
+    },
+);
 
 __PACKAGE__->setup;
 
-1;
+__PACKAGE__->meta->make_immutable;
+
--- t/lib/TestApp/Controller/Root.pm
+++ t/lib/TestApp/Controller/Root.pm
@@ -9,5 +9,7 @@ __PACKAGE__->config(namespace => q{});
 # your actions replace this one
 sub main :Path { $_[1]->res->body('<h1>It works</h1>') }
 
+sub end : ActionClass('RenderView') {}
+
 __PACKAGE__->meta->make_immutable;
 
