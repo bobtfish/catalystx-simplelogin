1aa1a3f179c5b434ccd358d01fb89a0fb2c9bdfe
t0m <bobtfish@bobtfish.net>
2009-07-13 21:19:55 +0100

    Make role choosing stuff work

--- lib/CatalystX/SimpleLogin/Controller/Login.pm
+++ lib/CatalystX/SimpleLogin/Controller/Login.pm
@@ -4,8 +4,7 @@ use namespace::autoclean;
 
 BEGIN { extends 'Catalyst::Controller::ActionRole'; }
 
-# Deal with different ways to use this controller here
-# FIXME - This is waaay bigger/uglier than it should be..
+# Deal with different ways to use this controller here. Yes, I apply the roles directly onto this class FIXME.
 sub COMPONENT {
     my ($class, $app, $args) = @_;
     my $login_prefix = 'CatalystX::SimpleLogin::ControllerRole::Login';
@@ -15,14 +14,17 @@ sub COMPONENT {
         : $login_prefix;
     # FIXME - This blows goats, if you say roles => [qw/Role1 Role2/],
     #         it doesn't play nice with methodattributes.
-    my $actual_class = Moose::Meta::Class->create_anon_class(
-        superclasses => [ $class->meta->superclasses ],
-    )->name();
-    Moose::Util::apply_all_roles($actual_class->meta, $login);
-    Moose::Util::apply_all_roles($actual_class->meta, 'CatalystX::SimpleLogin::ControllerRole::Logout');
-    $actual_class->meta->make_immutable;
-    $actual_class->new($c, $args);
+
+    Class::MOP::load_class($login);
+    $login->meta->apply($class->meta);
+
+    my $logout = 'CatalystX::SimpleLogin::ControllerRole::Logout';
+    Class::MOP::load_class($logout);
+    $logout->meta->apply($class->meta);
+
+    $class->meta->make_immutable;
+    $class->new($app, $args);
 }
 
-__PACKAGE__->meta->make_immutable;
+1;
 
