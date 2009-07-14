bb86193cd340b3f3c1579271ffa48dc9977db608
Gerda Shank <gerda.shank@gmail.com>
2009-07-14 19:37:57 -0400

    Add pod stub to NeedsLogin, remove tabs from TestAppRedirect

--- lib/Catalyst/ActionRole/NeedsLogin.pm
+++ lib/Catalyst/ActionRole/NeedsLogin.pm
@@ -2,6 +2,21 @@ package Catalyst::ActionRole::NeedsLogin;
 use Moose::Role;
 use namespace::autoclean;
 
+=head1 NAME
+
+Catalyst::ActionRole::NeedsLogin
+
+=head1 SYNOPSIS
+
+In a Catalyst controller:
+
+   sub some_method :Local :Does('NeedsLogin') {
+       my ($self, $c) = @_;
+       ....... 
+   }
+
+=cut
+
 around execute => sub {
 	my $orig = shift;
 	my $self = shift;
