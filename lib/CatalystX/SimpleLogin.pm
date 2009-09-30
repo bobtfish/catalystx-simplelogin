4bb7bcf4f84d606f36499937f21dc7d38a74e15e
Florian Ragwitz <rafl@debian.org>
2009-09-30 02:18:59 +0200

    Version 0.01.

--- lib/CatalystX/SimpleLogin.pm
+++ lib/CatalystX/SimpleLogin.pm
@@ -3,6 +3,8 @@ use Moose::Role;
 use CatalystX::InjectComponent;
 use namespace::autoclean;
 
+our $VERSION = '0.01';
+
 after 'setup_components' => sub {
     my $class = shift;
     CatalystX::InjectComponent->inject(
