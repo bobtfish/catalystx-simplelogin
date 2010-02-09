e381b1638b819d9477c46f7b09b73f7c5258e034
Tomas Doran <bobtfish@bobtfish.net>
2010-02-09 20:21:12 +0000

    Initial stab at code for tests

---
+++ t/lib/TestApp/Controller/ChainedExample.pm
@@ -0,0 +1,19 @@
+package TestApp::Controller::ChainedExample;
+use Moose;
+use namespace::autoclean;
+
+BEGIN { extends 'Catalyst::Controller::ActionRole' }
+
+sub base : Chained('/login/required') PathPart('') CaptureArgs(0) {} # Chain everything in the controller off of here.
+
+sub index : Chained('base') PathPart('') Args(0) { # /chainedexample
+}
+
+sub item : Chained('base') PathPart('') Args(1) { #/chainedexample/$arg1
+    my ($self, $c, $arg1) = @_;
+    $c->stash->{arg1} = $arg1;
+}
+
+sub public : Chained('/login/not_required') Args(0) {} # /chainedexample/public
+
+__PACKAGE__->meta->make_immutable;
