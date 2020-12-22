432d1cddb5059aa3c6d5591436669bac3d87f1bd
Alexander Hartmaier <abraxxa@cpan.org>
2020-12-22 12:15:02 +0100

    version 0.21

--- lib/CatalystX/SimpleLogin.pm
+++ lib/CatalystX/SimpleLogin.pm
@@ -3,7 +3,7 @@ use Moose::Role;
 use CatalystX::InjectComponent;
 use namespace::autoclean;
 
-our $VERSION = '0.20';
+our $VERSION = '0.21';
 
 after 'setup_components' => sub {
     my $class = shift;
